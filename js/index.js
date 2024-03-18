5f85037a8115cb6e345c43d58b48d56d8bf1580b
jayathracm <jayathracm@gmail.com>
2024-03-18 17:49:02 +0200

    part 3 done

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ const BACKEND_ROOT_URL  = 'http://localhost:3001'
 const list = document.querySelector('ul')
 const input = document.querySelector('input')
 
-input.disabled = true
+input.disabled = false
 
 const renderTask = (task) => {
   const li = document.createElement('li')
@@ -14,37 +14,51 @@ const renderTask = (task) => {
 
 const getTasks = async () => {
   try {
-    const response = await fetch(BACKEND_ROOT_URL)
-    const json = await response.json()
-    json.forEach(task => 
-      renderTask(task.description))
+      const response = await fetch(BACKEND_ROOT_URL);
+      const json = await response.json();
+      json.forEach(task => {
+          renderTask(task.description);
+      });
+      input.disabled = false;
   } catch (error) {
-    alert("Error retrieving tasks" + error.message)
+      console.error('Error:', error);
+      alert('Error fetching tasks' + error);
   }
 }
 
 const saveTask = async (task) => {
-  saveTask(task).then((json)=>{
-    renderTask(task)
-    input.value = ''
-  })
+  try {
+      const json = JSON.stringify({description: task});
+      const response = await fetch(BACKEND_ROOT_URL+ "/new", {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json'
+          },
+          body: json
+      });
+      return response.json();
+  } catch (error) {
+      console.error('Error:#', error);
+      alert('Error saving task#' + error.message);
+  }
 }
 
-input.addEventListener('keypress',(event) => {
-  if (event.key === 'Enter') {
-    event.preventDefault()
-    const task = input.value.trim()
-    if (task !== ''){
-        const li = document.createElement('li')
-        li.setAttribute('class', 'list-group-item')
-        li.innerHTML = task
-        list.appendChild(li)
-        input.value = ''
-    }
-    newItem.textContent = input.value;
-    list.appendChild(newItem);
-    input.value = '';
+getTasks()
+
+input.addEventListener('keypress', (e) => {
+  if(e.key === 'Enter') {
+      e.preventDefault();
+      const task = input.value.trim();
+      if (task !=='') {
+          const li = document.createElement('li');
+          li.setAttribute('class', 'list-group-item');
+          li.innerHTML = task;
+          list.appendChild(li);
+          input.value = '';
+          saveTask(task).then((json) => {
+              renderTask(task);
+              input.value = '';
+          });
+      } 
   }
 });
-
-getTasks()
